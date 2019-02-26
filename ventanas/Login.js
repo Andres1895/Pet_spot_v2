0e5be400e64a6dd6b57556c189301ebc3a9f64bb
Andres1895 <and.mg18@gmail.com>
2019-02-25 23:41:51 -0600

    styles for forms

---
+++ ventanas/Login.js
@@ -0,0 +1,59 @@
+import React, {Component} from 'react';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Platform,AppRegistry, StyleSheet, Text, View, TouchableOpacity, Scr} from 'react-native';
+import { Button, Input } from 'react-native-elements';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+
+class login extends React.Component{
+    render()
+    {
+        return(
+            <View style={styles.container}>
+                <View style={styles.title}>
+                    <Text style={{fontSize: 30, textAlign: 'center'}}>
+                        Inicia Sesión
+                    </Text>
+                </View>
+                <View >
+                    <Input containerStyle={styles.Inform}
+                        placeholder='Correo electronico/Usuario'
+                        leftIcon={{ type: 'font-awesome', name: 'user' }}
+                        textAlign= 'center'
+                    />
+                    <Input containerStyle={styles.Inform}
+                        placeholder='Contraseña'
+                        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                        secureTextEntry={true}
+                        textAlign= 'center'
+                    />
+                </View>
+                
+                <View style/>
+                <Button containerStyle={styles.Button}
+                title="Ingresa"
+                type="outline"
+                />
+
+            </View>
+        )
+    }
+}
+export default login;
+
+const styles = StyleSheet.create({
+    container:{
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'stretch',
+   },
+   title: {
+    fontSize: 30,
+    textAlign: 'center',
+    paddingTop: 60
+  },
+  Inform:{
+    paddingBottom: 15
+  }
+
+ })
--- ventanas/registro.js
+++ ventanas/registro.js
@@ -1,63 +1,76 @@
-import React, {Component} from 'react-native';
+import React, {Component} from 'react';
 import Icon from 'react-native-vector-icons/FontAwesome';
-import {Platform,AppRegistry, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Platform,AppRegistry, StyleSheet, Text, View, TouchableOpacity, ScrollView, TextInput} from 'react-native';
 import { Button, Input } from 'react-native-elements';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
+import t from 'tcomb-form-native';
+
 
 class registro extends React.Component{
     render(){
         return(
             <View style={styles.container}>
-                <Text style={styles.title}>
-                    Registrate
-                </Text>
-                <Input style={styles.Inform}
-                placeholder='Correo electronico'
-                leftIcon={{ type: 'font-awesome', name: 'user' }}
-                textAlign= 'center'
-                />
-                <Input style={styles.Inform}
-                placeholder='Contraseña'
-                leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                secureTextEntry={true}
-                textAlign= 'center'
-                />
-                <Input style={styles.Inform}
-                placeholder='Validación de contraseña'
-                leftIcon={{ type: 'font-awesome', name: 'lock' }}
-                secureTextEntry={true}
-                textAlign= 'center'
-                />
-
+                <View style={styles.title}>
+                    <Text style={{fontSize: 30, textAlign: 'center'}}>
+                        Registrate
+                    </Text>
+                </View>
+                <View style={{ flex: 1, flexDirection: 'column', justifyContent: 'space-evenly'}}>
+                    <ScrollView>
+                    <Input containerStyle={styles.Inform}
+                        placeholder='Nombre'
+                        leftIcon={{ type: 'font-awesome', name: 'user' }}
+                        textAlign= 'center'
+                    />
+                    <Input containerStyle={styles.Inform}
+                        placeholder='Correo electronico'
+                        leftIcon={{ type: 'font-awesome', name: 'user' }}
+                        textAlign= 'center'
+                    />
+                    <Input containerStyle={styles.Inform}
+                        placeholder='Contraseña'
+                        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                        secureTextEntry={true}
+                        textAlign= 'center'
+                    />
+                    <Input containerStyle={styles.Inform}
+                        placeholder='Validación de contraseña'
+                        leftIcon={{ type: 'font-awesome', name: 'lock' }}
+                        secureTextEntry={true}
+                        textAlign= 'center'
+                        errorStyle={{ color: 'red'}}
+                    />
+                    
+                    </ScrollView>
+                </View>
+                <View >
+                    <Button containerStyle={styles.Button}
+                    title="Ingresa"
+                    type="outline"
+                    />
+                </View>
             </View>
 
         )
     }
 }
 
-
-const BodyStack = createStackNavigator({
-    Registro: registro
-  });
-  
-  export default createAppContainer(BodyStack);
+  export default registro;
 
  const styles = StyleSheet.create({
     container:{
     flex: 1,
     flexDirection: 'column',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
     alignItems: 'stretch',
-    backgroundColor: '#F5FCFF',
    },
    title: {
     fontSize: 30,
     textAlign: 'center',
-    margin: 10,
+    padding: 50
   },
   Inform:{
-    color: '#ddd',
-    textAlign: 'center'
+    paddingBottom: 15
   },
 
  })
