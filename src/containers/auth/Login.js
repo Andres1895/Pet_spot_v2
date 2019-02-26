31b69fe750885f9ef8092254f5b53a1ee53ca56a
montse <montse94@gmail.com>
2019-02-26 04:40:48 -0600

    Modificaciones Login

--- src/containers/auth/Login.js
+++ src/containers/auth/Login.js
@@ -1,12 +1,38 @@
-import React, {Component} from 'react';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import {Platform,AppRegistry, StyleSheet, Text, View, TouchableOpacity, Scr} from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
 import { Button, Input } from 'react-native-elements';
-import { createStackNavigator, createAppContainer } from 'react-navigation';
 
 class Login extends React.Component{
-    render()
-    {
+    state = { 
+        errorMesagge:'',
+        userName: '',
+        password:''
+    }
+
+    verifyFields = () => {
+        const {userName, password} = this.state;
+        const { goBack }=this.props.navigation;
+
+        if(userName!== '' && password!=='' )
+        {
+            if (userName ==  'Andres' && password == '1234')
+            {
+                goBack();
+            }else{
+                this.state.userName='';
+                this.state.password='';
+                this.setState({
+                    errorMessage:'Campos incorrectos'
+                });
+            }
+          
+        }else{
+             this.setState({
+                errorMessage:'Campo obligatorio'
+            });
+        }
+    }
+    render(){
         return(
             <View style={styles.container}>
                 <View style={styles.title}>
@@ -19,18 +45,25 @@ class Login extends React.Component{
                         placeholder='Correo electronico/Usuario'
                         leftIcon={{ type: 'font-awesome', name: 'user' }}
                         textAlign= 'center'
+                        errorMessage= {this.state.userName !== '' ? '' : this.state.errorMessage}
+                        onChangeText={(text) => this.setState({userName:text})}
+                        value={this.state.userName}
                     />
                     <Input containerStyle={styles.Inform}
                         placeholder='Contraseña'
                         leftIcon={{ type: 'font-awesome', name: 'lock' }}
                         secureTextEntry={true}
                         textAlign= 'center'
+                        errorMessage= {this.state.password !== '' ? '' : this.state.errorMessage}
+                        onChangeText={(text) => this.setState({password:text})}
+                        value={this.state.password}
                     />
                 </View>
                 
                 <Button containerStyle={styles.Button}
-                title="Ingresa"
-                type="outline"
+                    title="Ingresa"
+                    type="outline"
+                    onPress={()=>this.verifyFields()}
                 />
 
             </View>
@@ -41,18 +74,18 @@ export default Login;
 
 const styles = StyleSheet.create({
     container:{
-    flex: 1,
-    flexDirection: 'column',
-    justifyContent: 'space-between',
-    alignItems: 'stretch',
-   },
-   title: {
-    fontSize: 30,
-    textAlign: 'center',
-    paddingTop: 60
-  },
-  Inform:{
-    paddingBottom: 15
-  }
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between',
+        alignItems: 'stretch',
+    },
+    title: {
+        fontSize: 30,
+        textAlign: 'center',
+        paddingTop: 60
+    },
+    Inform:{
+        paddingBottom: 15
+    }
 
  })
